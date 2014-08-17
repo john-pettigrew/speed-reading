42983d19aea7ce2dc7658503e7171e37b818d7ab
John Pettigrew <john.pettigrew404@gmail.com>
2014-08-16 22:24:41 -0500

    Add changing text; Add ability to change text speed.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,20 +8,34 @@ angular.module('starter.controllers', [])
 	}
 })
 
-.controller('ReadCtrl', function($scope, ReadData) {
+.controller('ReadCtrl', function($scope, $interval, ReadData) {
 	$scope.words = [];
+	var nextWordInt = 0;
 	var parseText = function(){
-		var currentWord = '';
+		var word = '';
 		for(var i=0; i < ReadData.text.length; i++){
 			if(ReadData.text.charAt(i) === ' '){
-				$scope.words.push(currentWord);
-				currentWord = '';
+				$scope.words.push(word);
+				word = '';
 			}
 			else{
-				currentWord = currentWord.concat(ReadData.text.charAt(i));
+				word = word.concat(ReadData.text.charAt(i));
 			}
 		}
 		console.log($scope.words);
 	}
+	var changeWord = function(){
+		console.log('changeWord');
+		$scope.currentWord = $scope.words[nextWordInt];
+		nextWordInt++;
+		if(nextWordInt >= $scope.words.length){
+			$interval.cancel(promise);
+			$scope.currentWord = '';
+		}
+	}
+	$scope.$on('$destroy', function(){
+		$interval.cancel(promise);
+	});
 	parseText();
+	var promise = $interval(changeWord, (3600 / ReadData.speed)*10);
 });
